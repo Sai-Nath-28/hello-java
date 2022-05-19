444c82a325284ef5921c299cc057969efdcdb981
Sai-Nath-28 <96463856+Sai-Nath-28@users.noreply.github.com>
2022-05-19 11:47:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, sainath");
-    System.out.println("Hello, sainath");
+    System.out.println("Hello, DOCKER");
     System.out.println("Hello, World");
 
   }
